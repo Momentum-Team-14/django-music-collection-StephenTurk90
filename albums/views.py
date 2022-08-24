238323a619cdd22e0624a6ec4254eaa26cd9af89
StephenTurk90 <stephenjturk@gmail.com>
2022-08-24 16:34:27 -0400

    attempt to get albums to show

--- albums/views.py
+++ albums/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Album
 
 
 def album_list(request):
+    albums = Album.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
     return render(request, 'albums/album_list.html', {})
