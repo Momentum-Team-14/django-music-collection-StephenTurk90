c31f9c54780f528151443c17618aa1ef65bb1b09
StephenTurk90 <stephenjturk@gmail.com>
2022-08-24 10:35:44 -0400

    added models and migrations

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
