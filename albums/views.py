32be725b91275b0d58bb15d4618d2c40a0b5d004
StephenTurk90 <stephenjturk@gmail.com>
2022-08-24 19:19:20 -0400

    show all albums using albums= Album.objects.all() and {'albums':albums}

--- albums/views.py
+++ albums/views.py
@@ -4,5 +4,5 @@ from .models import Album
 
 
 def album_list(request):
-    albums = Album.objects.filter(created_at__lte=timezone.now()).order_by('created_at')
-    return render(request, 'albums/album_list.html', {})
+    albums = Album.objects.all()
+    return render(request, 'albums/album_list.html', {'albums':albums})
