c2e7b0796d54d2107f781666cc2a4b1281307fed
StephenTurk90 <stephenjturk@gmail.com>
2022-08-24 11:01:36 -0400

    added albums and made a adminsuperuser

--- albums/models.py
+++ albums/models.py
@@ -1,7 +1,16 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
--- django_music/settings.py
+++ django_music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3&9h%qwjsbcke&edsm*#&cek#m3_)s3-tkk7bs71x%&03a*1w3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
